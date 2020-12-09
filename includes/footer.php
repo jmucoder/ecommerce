c0d81ba78465549a49505df58091cf63acf54242
Md Sayeed Alam <indianinjector@gmail.com>
2020-12-09 12:38:23 +0530

    first

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Dekh Bhai Mai banaya hun</b>
+      </div>
+      <strong>Copyright &copy; 2020 &nbsp<a href="http://ersayeeds.com">Er Sayeed</a></strong>
+    </div>
+</footer>
